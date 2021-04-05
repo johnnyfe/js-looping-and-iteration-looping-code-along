eef1a8df63670560b958de058f3065b53a1457bc
johnny <johnny1194@hotmail.com>
2021-04-04 23:56:06 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names= ["Johnny", "Kenneth","Beverly"];
+
+function writeCards(names,events='surprise'){
+    let thankYouCards =[]
+    for (let i = 0; i<names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return thankYouCards;
+}
+function countDown(numbers){
+    while (numbers>0){
+    console.log(numbers);
+    numbers-=1;
+    }
+    console.log(numbers);
+}
